6cf2d4ec198dd16be0aced341dc327b08ad861aa
Maomendi <maomendi98@gmail>
2025-07-25 18:06:57 -0500

    updated

--- week03/scripts/arrays.js
+++ week03/scripts/arrays.js
@@ -1,11 +1,23 @@
-let scores = [100, 72, 83, 94, 88, 87];
+let  numbers = [1,2,4,5,23, 56, 70 ,199, 400, 660, 230, 454];
 
-let names = ['Nancy','Blessing','Jorge','Svetlana'];
-
-names[1] = "Mauricio";
+const limit = 30
 
 
+//while loop
+//let n = 0
+//while (n < numbers.length) {
+  //if(numbers[n] > limit){
+  //  console.log(numbers[n])
+  //}
+  //n++
 
-scores.forEach(function(score) {
-    console.log(score);
-  });
+//} 
+
+
+// for loop 
+//for (let i = 0; i < numbers.length; i++){
+  //if (numbers[i] > limit){
+  //console.log(numbers[i])
+  //}
+//}
+
