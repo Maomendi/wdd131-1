7767618c2dfd17de2a20411139013e26e7ec5edc
Maomendi <maomendi98@gmail>
2025-07-27 21:41:57 -0500

    updated

--- week03/arrays.js
+++ week03/arrays.js
@@ -2,6 +2,7 @@ let  numbers = [1,2,4,5,23, 56, 70 ,199, 400, 660, 230, 454];
 
 const limit = 30
 
+console.log(numbers.lastIndex())
 
 //while loop
 //let n = 0
@@ -21,3 +22,19 @@ const limit = 30
   //}
 //}
 
+
+
+let scores = [100, 72, 83, 94, 88, 87];
+let accumulator = 0;
+let count = 0;
+scores.forEach(score => {
+  if (score > 87) {
+    accumulator += score;
+    count ++;
+  }
+});
+if ( count > 0 ) {
+  console.log(accumulator/count); 
+} else {
+  console.log("No scores reported.");
+}
