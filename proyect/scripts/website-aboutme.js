40fd5f53b06f0fbace3d68e1405eb7f53759b7bc
Maomendi <maomendi98@gmail.com>
2026-02-17 14:09:57 -0500

    updated

--- proyect/scripts/website-aboutme.js
+++ proyect/scripts/website-aboutme.js
@@ -62,14 +62,3 @@ function createTestimonialCards() {
         grid.appendChild(card);
     });
 }
-
-createTestimonialCards();
-
-
-const ham = document.getElementById("ham");
-const navMenu = document.getElementById("nav-menu");
-
-ham.addEventListener("click", () => {
-  ham.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
