96443c3d1b5b1c479d94d30792a622b8aeeeb80a
Maomendi <maomendi98@gmail.com>
2026-02-17 14:11:29 -0500

    udated

--- proyect/scripts/website-aboutme.js
+++ proyect/scripts/website-aboutme.js
@@ -1,3 +1,15 @@
+const ham = document.getElementById("ham");
+const navMenu = document.getElementById("nav-menu");
+
+ham.addEventListener("click", () => {
+  ham.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+
+
+
+
 // about me page
 
 const testimonials = [
